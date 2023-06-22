9c2018fc14b6ed038d3552f111b7e3aacc66d9f6
Shobu <shobu13@hotmail.fr>
2023-06-22 10:34:42 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <ol>
+    <li>test</li>
+  </ol>
+  <v-container style="background-color: blue;">
+    <p>
+      random lorem ipsum
+    </p>
+    <ul>
+      <li>test2</li>
+    </ul>
+    <ol>
+      <li>test</li>
+    </ol>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
